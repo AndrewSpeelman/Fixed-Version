d2a07b70d9c680f5a91fdf5baeb9baf0eb94d982
jjmjbj <sluhjbj@gmail.com>
2020-04-25 14:42:35 -0500

    level select added

---
+++ CPS Game v2/Assets/LevelSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void selectlevel(int level_index)
+    {
+        SceneManager.LoadScene(level_index);
+    }
+}
+
--- CPS Game v2/Assets/SceneLoader.cs
+++ CPS Game v2/Assets/SceneLoader.cs
@@ -21,4 +21,9 @@ public class SceneLoader : MonoBehaviour {
     {
         SceneManager.LoadScene(0);
     }
+
+    public void goToLS()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
 }
