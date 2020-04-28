f94cb9d80b18cf16b26849191b3367614d29ef70
Indiana Jancich <ijf8w@mst.edu>
2020-04-28 14:43:30 -0500

    Merged level selection with several behind-the-scenes fixes

--- CPS Game v2/Assets/SceneLoader.cs
+++ CPS Game v2/Assets/SceneLoader.cs
@@ -5,25 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
-	public void LoadNextScene()
+    public void LoadNextLevel()
     {
-        int current_scene_index = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(current_scene_index + 1);
-    }
-	
-	public void SkipScene()
-    {
-        int current_scene_index = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(current_scene_index + 2);
+        if(GameplayController.Level == GameplayController.NUM_LEVELS)
+        {
+            SceneManager.LoadScene(4);
+        }
+        else
+        {
+            GameplayController.Level = GameplayController.Level + 1;
+            //Five is the offset between the title screen and level 1
+            SceneManager.LoadScene(GameplayController.Level + 5);
+        }
     }
 
-    public void LoadStartScene()
+    public void LoadCurrentLevel()
     {
+        //Five is the offset between the title screen and level 1
+        SceneManager.LoadScene(GameplayController.Level + 5);
+    }
+
+    public void LoadLevel(int lvl)
+    {
+        GameplayController.Level = lvl;
+        //Five is the offset between the title screen and level 1
+        SceneManager.LoadScene(GameplayController.Level + 5);
+    }
+
+    public void LoadAttackerVictory()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadDefenderVictory()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadStartLevel()
+    {
+        GameplayController.Level = 0;
         SceneManager.LoadScene(0);
     }
 
-    public void goToLS()
+    public void LoadLevelSelect()
     {
-        SceneManager.LoadScene("Level Select");
+        SceneManager.LoadScene(5);
     }
 }
